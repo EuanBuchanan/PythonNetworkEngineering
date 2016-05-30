e2c2dd0fd21789fc9cabf8b80b9f63a0add3e4f0
EuanBuchanan <euan_b_oz@hotmail.com>
2016-05-30 16:26:24 +1000

    Udemy course start

---
+++ projects/SubnetCalculator/SubnetCalc_v1.py
@@ -0,0 +1,20 @@
+#   Learning code for Pything for Network Engineering.
+
+
+#   On taking an IP address and a subnet mask:
+
+#   Outuput:
+#       Network Address
+#       Broadcast Address
+#       Number of Valid hosts per subnet
+#       Wildcard Mask
+#       Mask bits
+
+#   Offer to generate random IP address for subnet? [y/n]
+#   Output Random IP address
+
+#   <continue>
+
+#   Refer application1.png for logical flow
+
+###############################################################################################
