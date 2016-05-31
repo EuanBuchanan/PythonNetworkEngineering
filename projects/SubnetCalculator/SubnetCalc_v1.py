c92191dfb06585b48d250b0ef732856ec458c5e6
EuanBuchanan <euan_b_oz@hotmail.com>
2016-06-01 06:47:29 +1000

    Collapsed functions in to a while True loop, enforced error checking.

--- projects/SubnetCalculator/SubnetCalc_v1.py
+++ projects/SubnetCalculator/SubnetCalc_v1.py
@@ -22,49 +22,39 @@
 import sys
 import random
 
-def ip_address_valid(ip):
-    
-    ''' This function carries out a number of tests to check that the IP is 
-    valid. '''
-    octets = ip.split('.')
-    def four_octets():
-        '''Check that there are four octets in the input.'''
-        return len(octets) == 4
-    def reserved_local():
-        return octets[0] == "127"
-    def multicast():
-        return octets[0] == "224"
-    def windows_default():
-        return octets[0] == "169" and octets[1] == "254"
-    def valid_octets():
-        return 1 <= int(octets[0]) <= 223 \
-        and 0 <= int(octets[1]) <= 255 \
-        and 0 <= int(octets[2]) <= 255 \
-        and 0 <= int(octets[3]) <= 255
-
-    if four_octets() == False:
-        print "The input needs to be four octets, for example 10.10.10.10, where the '.' seperates each octet."
-    elif reserved_local() == True:
-        print "Addresses beginning in '127' are reserved local IPs and not valid."
-    elif multicast() == True:
-        print "Addresses beginning in '224' are multicast addresses and not valid."
-    elif windows_default() == True:
-        print "Addresses beginning in '169.254' are Microsoft default addresses and not valid."
-    elif valid_octets() == False:
-        print "One or more octets is out of range. Please check and re-input."
-    else:
-        return octets
-
-
-
-
 def user_input_ip():
 
-    ''' The purpose of this function is to get an IP address form the user
-    and check that the IP is valid. '''
+    ''' This function carries out a number of tests to check that the IP is 
+    valid. '''
 
-    user_input = raw_input("Enter an IP address in the form of a.b.c.d: ")
-    return ip_address_valid(user_input)
+    while True:
+        user_input = raw_input("Enter an IP address in the form of a.b.c.d: ")
+        octets = user_input.split('.')
+        print len(octets)
+
+        if len(octets) != 4:
+            print "The input needs to be four octets, for example 10.10.10.10, where the '.' seperates each octet."
+            continue
+        elif octets[0] == "127":
+            print "Addresses beginning in '127' are reserved local IPs and not valid."
+            continue
+        elif octets[0] == "224":
+            print "Addresses beginning in '224' are multicast addresses and not valid."
+            continue
+        elif octets[0] == "169" and octets[1] == "254":
+            print "Addresses beginning in '169.254' are Microsoft default addresses and not valid."
+            continue
+        elif not (1 <= int(octets[0]) <= 223 \
+            and 0 <= int(octets[1]) <= 255 \
+            and 0 <= int(octets[2]) <= 255 \
+            and 0 <= int(octets[3]) <= 255):
+            print "One or more octets is out of range. Please check and re-input."
+            continue
+        else:
+            return octets
+            break
+
+
 
 ip_address = user_input_ip()
 print ip_address
