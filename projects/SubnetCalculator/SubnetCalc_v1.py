8213266fd2770b556cf394372aae97f87b8637e9
EuanBuchanan <euan_b_oz@hotmail.com>
2016-06-02 17:50:09 +1000

    Nearly complete, just random IP to do.

--- projects/SubnetCalculator/SubnetCalc_v1.py
+++ projects/SubnetCalculator/SubnetCalc_v1.py
@@ -37,7 +37,6 @@ def user_input_ip():
     while True:
         user_input = raw_input("Enter an IP address in the form of a.b.c.d: ")
         octets = user_input.split('.')
-        print len(octets)
 
         if len(octets) != 4:
             print "The input needs to be four octets, for example 10.10.10.10, where the '.' seperates each octet."
@@ -69,32 +68,100 @@ def subnet_check():
     while True:
 
         user_subnet = raw_input("Please enter subnet: ")
-        subnet_octets = user_subnet.split('.')
-        if len(subnet_octets) != 4:
+        ip_mask = user_subnet.split('.')
+        if len(ip_mask) != 4:
             print "Subnet needs to be entered as x.x.x.x, three '.' seperating the four octets."
 
-        elif subnet_octets[0] not in valid_octets or\
-             subnet_octets[1] not in valid_octets or\
-             subnet_octets[2] not in valid_octets or\
-             subnet_octets[3] not in valid_octets:
+        elif ip_mask[0] not in valid_octets or\
+             ip_mask[1] not in valid_octets or\
+             ip_mask[2] not in valid_octets or\
+             ip_mask[3] not in valid_octets:
             print "One or more octets is not valid. Valid octets are: "
             for octet in valid_octets:
                 print octet
             continue
 
-        elif subnet_octets[0] <= subnet_octets[1] <= subnet_octets[2] \
-            <= subnet_octets[3]:
+        elif ip_mask[0] <= ip_mask[1] <= ip_mask[2] \
+            <= ip_mask[3]:
             print "Each subsequent subnet octet from the first must be equal to or lesser than the previous"
             continue 
             
         else:
-            return subnet_octets
+            return ip_mask
     
+
+def convert_to_binary(octet_list):
+
+    binary_list = [ bin(int(octet) % 256)[2:].zfill(8) for octet in octet_list ]
+
+    return binary_list
+
+def get_network_broadcast(ip_address, ip_mask):
+    # This function takes two lists, string representations of the decimal values of the
+    # ip address and the subnet mask
+
+    # Using binary operations, the network address, the broacast address, the wildcard mask
+    # and the number of hosts are obtained.
+
+    ip_network_address = []
+    ip_wildcard_mask =[]
+    ip_broadcast_address = []
+    ip_mask_binary = convert_to_binary(ip_mask)
+    host_bits = 0
+
+
+    for octet in range(len(ip_mask)):
+
+        mask = int(ip_mask[octet])
+
+        # a subnet mask always has the rightmost bits as zeros, the number of zeros determines
+        # how many hosts there are in the network. The formulae for this is ( n ** 2 -2) where
+        # n is the number of zeroes
+
+        host_bits += ip_mask_binary[octet].count('0')
+
+        # The wildcard mask is the invers of the mask. Each octet is eight bits, therefore the
+        # sub of the sum of 256 and the inverse of the mask will give us the mask value.
+
+        wildcard = 256 + ~int(ip_mask[octet])
+
+        # The network address is obtained from a bitwsie and of the host address and the mask
+
+        host = int(ip_address[octet])
+        network_add = mask & host
+
+        # The sum of the nework addrss and the wildcard mask gives the broadcast address
+
+        broadcast_add = network_add + wildcard
+
+        ip_network_address.append(str(network_add))
+        ip_wildcard_mask.append(str(wildcard))
+        ip_broadcast_address.append(str(broadcast_add))
+
+
+    if host_bits == 1:
+        number_of_hosts = '1'
+    else:
+        number_of_hosts = str( 2 ** host_bits - 2)
+
+    print "The Network Address is: " + ".".join(ip_network_address)
+    print "The Wildcard Mask is: " + ".".join(ip_wildcard_mask)
+    print "The Broadcast Address is: " + ".".join(ip_broadcast_address)
+    print "The number of hosts is: " + str(number_of_hosts)
+
+    return (ip_network_address, ip_wildcard_mask, ip_broadcast_address, number_of_hosts)
+
+
+
 
 
 ip_address = user_input_ip()
-print ip_address
+ip_address_binary = convert_to_binary(ip_address)
 
-subnet_octets = subnet_check()
-print subnet_octets
+ip_mask = subnet_check()
+ip_mask_binary = convert_to_binary(ip_mask)
+
+ip_network_address, ip_wildcard_mask, ip_broadcast_address, number_of_hosts \
+        = get_network_broadcast(ip_address, ip_mask)
+
 
